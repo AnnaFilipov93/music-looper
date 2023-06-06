e3de2cfea76025bd5e805a9d6caa5684f61a9449
Anna Filipov <filipovanna93@gmail.com>
2023-06-06 11:44:56 +0300

    Added tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+
 import { Routes, Route } from 'react-router-dom';
 import Home from './page/Home.jsx'
 import Board from './page/Board';
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex text-center items-center justify-center h-full w-full">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/board" element={<Board />} />
--- src/page/Board.jsx
+++ src/page/Board.jsx
@@ -1,7 +1,22 @@
 const Board = () => {
     return (
-        <div className="font-bold">
-            Board
+        <div class="box-border h-full w-800 p-4 border-4 divide-y items-center justify-items-center">
+
+            {/* Header */}
+            <div className="p-4">
+                01
+            </div>
+
+            {/* Pads */}
+            <div className="p-4">
+                02
+            </div>
+
+            {/* Controller */}
+            <div className="p-4">
+                03
+            </div>
+
         </div>
     )
 }
