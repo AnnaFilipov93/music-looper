673c3890d6c375ef2925d01c23621c26ce98c88e
Anna Filipov <filipovanna93@gmail.com>
2023-06-18 15:14:10 +0300

    Added server

---
+++ server/Users.js
@@ -0,0 +1,53 @@
+const users = [
+
+    {
+  
+      id: 1,
+  
+      name: "John",
+  
+      email: "john@gmail.com"
+  
+    },
+  
+    {
+  
+      id: 2,
+  
+      name: "Smith",
+  
+      email: "smith@gmail.com"
+  
+    },
+  
+    {
+  
+      id: 3,
+  
+      name: "Chris",
+  
+      email: "chris@gmail.com"
+  
+    },
+  
+    {
+  
+      id: 4,
+  
+      name: "Jack",
+  
+      email: "jack@gmail.com"
+  
+    },
+    
+    {
+        id: "08066ee3-6b05-4c29-b16b-07acd4a951dd",
+
+        name: "John",
+
+        email: "john@gmail.com"
+    }
+  
+  ];
+  
+  module.exports = users;
